e81d86c5900e5eb86efca17c998a5c9643840afd
Ramiro Cosa <ramirocosa@gmail.com>
2024-10-03 16:39:54 -0300

    otro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { remarkReadingTime } from './src/utils/readingTime';
 import rehypePrettyCode from 'rehype-pretty-code';
-import vercelStatic from '@astrojs/vercel/static';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 const options = {
@@ -31,5 +30,12 @@ const options = {
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://astro-tech-blog-ten.vercel.app/',
+	markdown: {
+		syntaxHighlight: false,
+		// Disable syntax built-in syntax hightlighting from astro
+		rehypePlugins: [[rehypePrettyCode, options]],
+		remarkPlugins: [remarkReadingTime]
+	},
 	integrations: [tailwind(), react(), sitemap()]
 });
--- dist/pages/posts/first_blog_post.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../../chunks/first_blog_post_41gt6UFg.mjs';
-export { renderers } from '../../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
